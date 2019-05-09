893dc82d7f85d6a0e6793a3b804e80b847669492
qulu <793796269@qq.com>
2019-05-09 10:38:58 +0800

    no message

--- love-server/routes/services.js
+++ love-server/routes/services.js
@@ -1,7 +1,7 @@
 // 页面层 服务管理
 var express = require('express');
 var router = express.Router();
-const { addServices} = require("../service/servicesServive");
+const { addServices,getServices,removeServiceById,getServicesByPage} = require("../service/servicesServive");
   
 /* GET home page. */
 router.post('/addServices',async function(req, res, next) {
@@ -13,9 +13,7 @@ router.post('/addServices',async function(req, res, next) {
 router.get('/getServices', async function (req, res, next) {
   res.send(await getServices(res.query))
 });
-// router.get("/getShopsByPage", async function (req, res, next) {
-//   res.send(await getShopsByPage(req.query));
-// });
+
 
 //，根据id删除服务
 router.post('/removeServiceById', async function (req, res, next) {
@@ -23,7 +21,7 @@ router.post('/removeServiceById', async function (req, res, next) {
   res.send(await removeServiceById(req.body));
 })
 
-//按分页进行查询goods
+//按分页进行查询
 router.get('/getServicesByPage', async function (req, res, next) {
   console.log(req.query)
   res.send(await getServicesByPage(req.query))
